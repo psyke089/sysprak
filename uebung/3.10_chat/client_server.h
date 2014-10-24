bc579d0a54ffec08c55b16865dcbbedb6222a4ba
psyke089 <psyke089@gmail.com>
2014-10-24 18:11:44 +0200

    added header file

---
+++ uebung/3.10_chat/client_server.h
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h> 
+#include <netinet/in.h>
+#include <unistd.h>
+#include <strings.h>
+
+#define PORT 4711
+#define MSGL 256
