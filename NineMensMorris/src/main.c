0161dec2bb7872d26ebe1bd717abf727a7a3f1c3
cirquit <alex.isenko@googlemail.com>
2014-11-06 16:56:17 +0100

    added check for length gameid && automatic gameid creation

--- NineMensMorris/src/main.c
+++ NineMensMorris/src/main.c
@@ -4,11 +4,13 @@ char *id = NULL, *to = NULL;
 
 
 void printHowToUse (){
-  printf("NAME:                                              \n"
+  printf("\n"
+         "NAME:                                              \n"
          "           client - Unser 'Nine Mens Morris' client\n"
          "OPTIONS:                                           \n"
          "          -i Game-ID                               \n"
-         "          -a PLACEHOLDER                         \n\n");
+         "          -a PLACEHOLDER                           \n"
+         "\n");
   exit(0);
 }
 
@@ -17,26 +19,33 @@ void printHowToUse (){
  * ansonsten terminiert das Programm
  *
  * -i ist die Game-ID und muss genau 11 Zeichen lang sein
+ *    pauschal auf 12345678900
  * -a ist ein Platzhalter
  */
 void parseArgs(int argc, char *argv[]){
 
-  int pArg; 
-
-  while ((pArg=getopt(argc, argv, "i:a:")) != -1) {
-       switch (pArg) {
-         case 'i':
-            id = optarg; break;
-         case 'a':
-            to = optarg; break;
-         default:
-            printHowToUse(); break;
-      }
+  if (argc <= 1){
+    id = "12345678900";
+    printf("\nDie Game-ID wurde automatisch auf 12345678900 gesetzt.\n");
   }
+  else {
 
-  if (strlen(id) != 11){
-    printf ("Die Länge der Game-ID muss 11 Zeichen lang sein!");
-    exit(0);
+     int pArg; 
+     while ((pArg=getopt(argc, argv, "i:a:")) != -1) {
+         switch (pArg) {
+             case 'i':
+                id = optarg; break;
+             case 'a':
+                to = optarg; break;
+             default:
+                printHowToUse(); break;
+          }
+     }
+  
+      if (id != NULL && strlen(id) != 11){
+          printf ("Die Länge der Game-ID muss 11 Zeichen lang sein!\n");
+          exit(0);
+      }
   }
 }
 
@@ -45,7 +54,6 @@ void parseArgs(int argc, char *argv[]){
 int main(int argc, char *argv[]) { 
 
   parseArgs(argc, argv);
-
   return 0;
 }
 
