f0406c0e5722666ef443e921ac2211d23177b0be
psyke089 <psyke089@gmail.com>
2014-11-07 15:49:32 +0100

    performConnection update

--- NineMensMorris/src/connector/performConnection.c
+++ NineMensMorris/src/connector/performConnection.c
@@ -4,10 +4,50 @@
 #include <netinet/in.h>
 #include <strings.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 
-#include "main.h"
+#include "../main.h"
 
 #define MSGL 256
+
+/**
+ *  Löst HOSTNAME in eine Ip auf und
+ *  erstellt und initialisiert ein sockaddr_in struct daraus. 
+ *
+ *  returns: struct sockaddr_in
+ */
+
+struct sockaddr_in init_server_addr()
+{
+  
+  struct hostent *hp = gethostbyname(HOSTNAME);
+  struct sockaddr_in ret;
+
+  if (hp == NULL) {
+    printf("gethostbyname() failed\n");
+    exit(1);
+  }
+
+  // server_addr initilaisieren
+  bzero((char *) &ret, sizeof(ret));
+  
+  //Server-IP in sockaddr_in struct kopieren
+  memcpy(&ret.sin_addr, hp->h_addr_list[0], hp->h_length);
+
+  ret.sin_family = AF_INET;
+  ret.sin_port = htons( PORTNUMBER );
+
+  printf("%s = ", hp->h_name);
+  printf( "%s\n", inet_ntoa( *( struct in_addr*)( &ret.sin_addr )));
+
+  return ret;
+}
+
+/**
+ *  Erstellt einen Socket.
+ *
+ *  returns: Socket filedescriptor
+ */
 
 int create_socket()
 {
@@ -22,28 +62,12 @@ int create_socket()
     return fd;
 }
 
-void get_message(int sock, char* msg)
-{
-    int n;
-    n = read(sock, msg, MSGL);
-    if (n < 0)
-    {
-        printf("ERROR reading from socket");
-        exit(1);
-    }
-}
-
-void send_message(int sock, char* msg)
-{
-    int n;
-    n = write(sock, msg, MSGL);
-    if (n < 0)
-    {
-        printf("ERROR writing to socket");
-        exit(1);
-    }
-
-}
+/**
+ *  Connectet zu einem Socket.
+ *
+ *  sock: Socket filedescriptor
+ *  dest: sockaddr_in
+ */
 
 void connect_to_socket(int sock, struct sockaddr_in dest)
 {
@@ -57,55 +81,90 @@ void connect_to_socket(int sock, struct sockaddr_in dest)
     printf("connected to socket...\n");
 }
 
-int main (int argc, char* const argv[])
+/**
+ *  Liest von einem Socket.
+ *
+ *  sock: Socket filedescriptor
+ *  buf:  Pointer auf buffer, 
+ *        in den die erhaltene Nachricht geschrieben werden soll
+ */
+
+void get_message(int sock, char* buf)
 {
-    int socket_client;
-    struct sockaddr_in dest;
-
-    char in_buf[MSGL];
-    char out_buf[MSGL];
-
-    bzero(in_buf, MSGL);
-    bzero(out_buf, MSGL);
-
-
-    socket_client = create_socket();
-
-
-    bzero((char *) &dest, sizeof(dest));
-    dest.sin_addr.s_addr = inet_addr( argv[1] );
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons( PORTNUMBER );
-
-
-    connect_to_socket(socket_client, dest);
-
-
-    while(1)
+    int n;
+    n = read(sock, buf, MSGL);
+    if (n < 0)
     {
-        //get input
-        printf("Message to server: ");
-        bzero(out_buf, MSGL);
-        fgets(out_buf, MSGL, stdin);
-        if (strcmp(out_buf,"quit\n") == 0){
-            break;
-        }
+        printf("ERROR reading from socket");
+        exit(1);
+    }
+}
 
-        //send input
-        printf("sending: %s\n", out_buf);
-        send_message(socket_client, out_buf);
+/**
+ *  Schreibt in einen Socket.
+ *
+ *  sock: Socket filedescriptor
+ *  buf:  Pointer auf buffer mit zu schreibender Nachricht
+ */
 
-        //wait for reply
-        printf("waiting for reply...\n");
-        bzero(in_buf, MSGL);
-        get_message(socket_client, in_buf);
-        printf("server sayz: %s\n", in_buf);
+void send_message(int sock, char* buf)
+{
+    int n;
+    n = write(sock, buf, MSGL);
+    if (n < 0)
+    {
+        printf("ERROR writing to socket");
+        exit(1);
     }
 
+}
 
-    close(socket_client);
+int main(int argc, char const *argv[])
+{
+  
+
+  int le_socket;
+  struct sockaddr_in server_addr;
+
+  char in_buf[MSGL];
+  char out_buf[MSGL];
+
+  bzero(in_buf, MSGL);
+  bzero(out_buf, MSGL);
 
 
-    return 0;
+  printf("\n");
 
+  server_addr = init_server_addr();
+
+  le_socket = create_socket();
+
+  connect_to_socket(le_socket, server_addr);
+
+  printf("\n");
+  while(1)
+  {
+    
+    //warten auf Nachricht vom Server
+    bzero(in_buf, MSGL);
+    get_message(le_socket, in_buf);
+    printf("%s\n", in_buf);
+
+    //get input
+    printf("> ");
+    bzero(out_buf, MSGL);
+    fgets(out_buf, MSGL, stdin);
+    printf("\n");
+    if (strcmp(out_buf,"quit\n") == 0){
+        break;
+    }
+
+    //send input
+    send_message(le_socket, out_buf);
+
+  }
+
+  close(le_socket);
+
+  return 0;
 }
