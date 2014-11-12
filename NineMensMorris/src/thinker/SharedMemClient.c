838d3203c12f35cd1edb68eee2f8f4799052cda1
psyke089 <psyke089@gmail.com>
2014-11-12 17:46:01 +0100

    removed whitespaces ;D

--- NineMensMorris/src/thinker/SharedMemClient.c
+++ NineMensMorris/src/thinker/SharedMemClient.c
@@ -4,21 +4,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
- 
+
 #define SHMSZ     1024
- 
+
 main()
 {
   int shmid;
   key_t key;
   char *shm, *s;
- 
+
   /*
   * We need to get the segment named
   * "1234", created by the server.
   */
   key = 1234;
- 
+
   /*
   * Locate the segment.
   */
@@ -26,7 +26,7 @@ main()
     perror("shmget");
     return 1;
   }
- 
+
   /*
   * Now we attach the segment to our data space.
   */
@@ -34,13 +34,13 @@ main()
     perror("shmat");
     return 1;
   }
- 
+
   /*
   * Zero out memory segment
   */
   memset(shm,0,SHMSZ);
   s = shm;
- 
+
   /*
   * Client writes user input character to memory
   * for server to read.
@@ -49,16 +49,16 @@ main()
     char tmp = getchar();
     // Eat the enter key
     getchar();
- 
+
     if(tmp == 'q'){
       *shm = 'q';
       break;
     }
     *shm = tmp;
   }
- 
+
   if(shmdt(shm) != 0)
     fprintf(stderr, "Could not close memory segment.\n");
- 
+
   return 0;
 }
