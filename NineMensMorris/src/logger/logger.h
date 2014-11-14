a829286d00cc597551df77ce18e40aa0a54da1e8
tmkr <tim.krau@gmail.com>
2014-11-14 06:16:45 +0100

    nützliche fkt aus logger.c in logger.h, @todo doku anpassen

--- NineMensMorris/src/logger/logger.h
+++ NineMensMorris/src/logger/logger.h
@@ -1,1 +1,10 @@
+
 void logPrnt (char c, char t, char* input);
+
+void initLogSession();
+
+char* getTimeAsString (char p);
+
+void prntColor(char c, char* input );
+
+///@todo das mit dem initLogSession noch besser planen
