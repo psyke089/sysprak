5bdf6e2e85fabd52a03e568cd6638c5521856077
Zen5656 <aasdad>
2014-11-18 16:41:53 +0100

    Config files created

---
+++ NineMensMorris/src/config.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "config.h"
+
+void testi(){
+	printf("test");
+	exit(0);
+}
---
+++ NineMensMorris/src/config.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+
+
+
+void testi();
